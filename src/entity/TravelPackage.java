c42b0ca9acdbffdc3a52105ffa73e9dcb2e00ea1
DhyeyaPatel <pateldhyeya7@gmail.com>
2024-01-02 23:09:29 +0530

    Added: (1)TravelPackage class

---
+++ src/entity/TravelPackage.java
@@ -0,0 +1,53 @@
+package entity;
+
+import java.util.List;
+
+public class TravelPackage {
+    private String name;
+    private int passengerCapacity;
+    private List<Destination> itinerary;
+    private List<Passenger> passengersList;
+
+    public TravelPackage() {
+        super();
+    }
+
+    public TravelPackage(String name, int passengerCapacity, List<Destination> itinerary, List<Passenger> passengersList) {
+        this.name = name;
+        this.passengerCapacity = passengerCapacity;
+        this.itinerary = itinerary;
+        this.passengersList = passengersList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public void setPassengerCapacity(int passengerCapacity) {
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    public List<Destination> getItinerary() {
+        return itinerary;
+    }
+
+    public void setItinerary(List<Destination> itinerary) {
+        this.itinerary = itinerary;
+    }
+
+    public List<Passenger> getPassengersList() {
+        return passengersList;
+    }
+
+    public void setPassengersList(List<Passenger> passengersList) {
+        this.passengersList = passengersList;
+    }
+}
